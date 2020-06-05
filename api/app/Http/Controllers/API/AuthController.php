48fe29017bc029368c46bdbd1d9c465c3b3d834b
Marcelo <teixeira@eida.lu>
2020-06-05 12:40:50 +0200

    API START BUILD

---
+++ api/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Http\Controllers\API;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+class AuthController extends Controller
+{
+    /**
+     * Login API
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request){
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('LaraPassport')->accessToken;
+            return response()->json([
+                'status' => 'success',
+                'data' => $success
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'data' => 'Unauthorized Access'
+            ]);
+        }
+    }
+
+    /**
+     * Register API
+     * PACKAGE : NOT_SELECT|PACKAGE_1|PACKAGE_2|PACKAGE_3
+     * ROLE : ADMIN|USER
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            //'package' => 'required',
+            'role' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()]);
+        }
+        $postArray = $request->all();
+        $postArray['password'] = bcrypt($postArray['password']);
+        $postArray['package'] =  empty($postArray['package']) ? 'NOT_SELECT' : $postArray['package'] ;
+        $postArray['role'] =  empty($postArray['role']) ? 'USER' : 'ADMIN';
+        $user = User::create($postArray);
+        $success['token'] =  $user->createToken('LaraPassport')->accessToken;
+        $success['name'] =  $user->name;
+        $success['package'] =  empty($user->package) ? 'NOT_SELECT' : $user->package ;
+        $success['role'] =  empty($user->role) ? 'USER' : 'ADMIN';
+        return response()->json([
+            'status' => 'success',
+            'data' => $success,
+        ]);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('login', 'API\AuthController@login');
+Route::post('register', 'API\AuthController@register');
