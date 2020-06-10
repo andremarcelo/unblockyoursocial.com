a6b59f103b89b77e3a13a39c6ec650befcbd9159
Marcelo <teixeira@eida.lu>
2020-06-10 18:07:50 +0200

    Register logic

--- api/routes/web.php
+++ api/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('send_test_email', function(){
+    Mail::raw('Sending emails with Mailgun and Laravel is easy!', function($message)
+    {
+        $message->subject('Mailgun and Laravel are awesome!');
+        $message->from('no-reply@mg.unblockyoursocial.com', 'Website Name');
+        $message->to('marceo.t80@gmail.com');
+    });
+});
